b370d5a8db4ae1a606f81470d57766f5be62506f
Mateo55Dev <mateusz.rozenek121@gmail.com>
2024-01-26 22:01:17 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import Bmi from './components/Bmi.vue';
+import './assets/styles/style.css';
+
+// const activity = ref({});
+// const result = ref({});
+
+// const fetchData = async () => {
+//   const options = {
+//     method: 'GET',
+//     url: 'https://fitness-calculator.p.rapidapi.com/burnedcalorie',
+//     params: {
+//       activityid: 'bi_1',
+//       activitymin: '25',
+//       weight: '75',
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'your-api-key',
+//       'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com',
+//     },
+//   };
+
+//   try {
+//     const response = await axios.request(options);
+//     const responseData = response.data || {};
+    
+//     activity.value = responseData.activity || {};
+    
+//     result.value = {
+//       calories_burned: responseData.data.burnedCalorie,
+//       unit: responseData.data.unit,
+//     };
+    
+//     console.log(responseData);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// onMounted(() => {
+//   fetchData();
+// });
+</script>
+
+<template>
+  <div class="container">
+    <!-- <header>
+      <h2>Wywołaj API dla ilości spalonych kalorii</h2>
+    </header>
+
+    <div class="data">
+      <p>ID aktywności: {{ result.id }}</p>
+      <p>Nazwa aktywności: {{ activity.name }}</p>
+      <p>Spalone kalorie: {{ result.calories_burned }} {{ result.unit }}</p>
+    </div> -->
+    
+    <Bmi/>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background: grey;
+  padding: 20px;
+
+  .data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ src/components/Calories.vue
@@ -0,0 +1,47 @@
+<script setup>
+import axios from 'axios';
+
+const options = {
+    method: 'GET',
+    url: 'https://fitness-calculator.p.rapidapi.com/burnedcalorie',
+    params: {
+        activityid: 'bi_1',
+        activitymin: '25',
+        weight: '75'
+    },
+    headers: {
+        'X-RapidAPI-Key': 'your-api-key',
+        'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+    }
+};
+
+let activity = {};
+let result = {};
+
+try {
+    const response = await axios.request(options);
+    activity = response.data.activity || {};
+    result = response.data.result || {};
+    console.log(response.data);
+} 
+catch (error) {
+    console.error(error);
+}
+</script>
+
+<template>
+    <div>
+        <header>
+            <h2>Call API for calories amount</h2>
+        </header>
+        <div>
+            <p>Activity ID: {{ activity.id }}</p>
+            <p>Activity Name: {{ activity.name }}</p>
+            <p>Calories Burned: {{ result.calories_burned }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
